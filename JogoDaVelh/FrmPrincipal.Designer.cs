d42d825593ac0cd72aab7ef08a4bbff5c4ae7c5d
Alberto Monteiro <betimbrasil@gmail.com>
2011-10-16 22:44:40 -0300

    tratamento de erro quando houver falha na conexao - reiniciar jogo implementado

--- JogoDaVelh/FrmPrincipal.Designer.cs
+++ JogoDaVelh/FrmPrincipal.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace JogoDaVelha
 {
-    partial class Form1
+    partial class FrmPrincipal
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
             this.lbl1 = new System.Windows.Forms.Label();
             this.lbl2 = new System.Windows.Forms.Label();
             this.lbl3 = new System.Windows.Forms.Label();
--- JogoDaVelh/FrmPrincipal.cs
+++ JogoDaVelh/FrmPrincipal.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
+using System.Net.Sockets;
 using System.Windows.Forms;
 
 namespace JogoDaVelha
 {
-    public partial class Form1 : Form
+    public partial class FrmPrincipal : Form
     {
         //comentario teste
         private Label[,] matriz;
@@ -19,7 +16,7 @@ namespace JogoDaVelha
         private bool online;
         private string meuSimbolo;
 
-        public Form1()
+        public FrmPrincipal()
         {
             InitializeComponent();
         }
@@ -52,34 +49,48 @@ namespace JogoDaVelha
                 bool resultado = AlguemVenceu();
                 if (online)
                 {
-                    int i = 0, j = 0;
-                    bool found = false;
-                    for (; i < 3; i++)
-                    {
-                        for (j = 0; j < 3; j++)
-                        {
-                            found = matriz[i, j] == lbl;
-                            if (found) break;
-                        }
-                        if (found) break;
-                    }
-                    comunicador.InformarJogada(i, j, txtNome.Text, resultado);
-                    LiberaQuadrados(false);
-                    if (resultado)
-                        MessageBox.Show("Parabéns otário", "Jogo da velha", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    InformaJogada(lbl, resultado);
                 }
-                if (!online)
+                if(!online)
+                {
                     if (!resultado)
                     {
                         TurnoDaMaquina();
                         if (AlguemVenceu())
-                            MessageBox.Show("Se fudeu otário", "Jogo da velha", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            MessageBox.Show("Não foi dessa vez, você perdeu", "Jogo da velha", MessageBoxButtons.OK, MessageBoxIcon.Information);
                     }
                     else
-                        MessageBox.Show("Parabéns otário", "Jogo da velha", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        MessageBox.Show("Parabéns você venceu!", "Jogo da velha", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                if (DeuVelha(resultado))
+                    MessageBox.Show("Empante, ninguem venceu.", "Jogo da velha", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             else
                 MessageBox.Show("Escolha outro quadrado", "Jogo da velha", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private bool DeuVelha(bool resultado)
+        {
+            return labels.All(x => !string.IsNullOrEmpty(x.Text)) && !resultado;
+        }
+
+        private void InformaJogada(Label lbl, bool resultado)
+        {
+            int i = 0, j = 0;
+            bool found = false;
+            for (; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    found = matriz[i, j] == lbl;
+                    if (found) break;
+                }
+                if (found) break;
+            }
+            comunicador.InformarJogada(i, j, txtNome.Text, resultado);
+            LiberaQuadrados(false);
+            if (resultado)
+                MessageBox.Show("Parabéns você venceu!", "Jogo da velha", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private bool AlguemVenceu()
@@ -104,9 +115,12 @@ namespace JogoDaVelha
                 controle != label3);
             Random random = new Random();
 
-            var indice = random.Next(disponiveis.Count());
+            if (disponiveis.Any())
+            {
+                var indice = random.Next(disponiveis.Count());
 
-            disponiveis.ElementAt(indice).Text = "O";
+                disponiveis.ElementAt(indice).Text = "O"; 
+            }
         }
 
         private bool ChecaLinhasOuColunas(bool ehLinha)
@@ -119,7 +133,7 @@ namespace JogoDaVelha
                 int i = 0;
                 for (; i < 2; i++)
                     resultado = (ehLinha) ?
-                        resultado && matriz[item, i].Text == matriz[item, i + 1].Text && !string.IsNullOrWhiteSpace(matriz[i, item].Text) :
+                        resultado && matriz[item, i].Text == matriz[item, i + 1].Text && !string.IsNullOrWhiteSpace(matriz[item, i].Text) :
                         resultado && matriz[i, item].Text == matriz[i + 1, item].Text && !string.IsNullOrWhiteSpace(matriz[i, item].Text);
                 if (resultado && i <= 2)
                     break;
@@ -144,43 +158,57 @@ namespace JogoDaVelha
         private void RecebiMensagem(Mensagem mensagem)
         {
             if (mensagem.Tipo == TipoMensagem.JogadaEfetuada)
-            {
-                int x = mensagem.Posicao.Key, y = mensagem.Posicao.Value;
-                PreencheQuadrado(x, y);
-                if (mensagem.EuVenci)
-                {
-                    MessageBox.Show(string.Format("Se fudeu otário\n{0} ganhou de você.", mensagem.NomeJogador), "Jogo da velha", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    LiberaQuadrados(false);
-                }
-                else
-                    LiberaQuadrados(true);
-            }
+                JogadaEfetuada(mensagem);
             else if (mensagem.Tipo == TipoMensagem.SolicitacaoDeJogo)
-            {
-                var retorno = MessageBox.Show(string.Format("{0} deseja jogar com você!\nVocê aceita?", mensagem.NomeJogador), "Jogo da Velha", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-
-                if (retorno == DialogResult.Yes)
-                {
-                    comunicador.AceitarJogo(true, txtNome.Text);
-                    IniciaJogo(true);
-                    meuSimbolo = "O";
-                    online = true;
-                }
-                else
-                    comunicador.AceitarJogo(false, txtNome.Text);
-            }
+                SolicitacaoDeJogo(mensagem);
             else if (mensagem.Tipo == TipoMensagem.JogoAceito && aguardandoResposta)
-            {
-                aguardandoResposta = false;
-                MessageBox.Show(string.Format("{0} aceitou o desafio", mensagem.NomeJogador), "Jogo da Velha");
-                online = true;
-            }
+                JogoAceito(mensagem);
             else if (mensagem.Tipo == TipoMensagem.JogoRecusado && aguardandoResposta)
+                JogoRecusado(mensagem);
+        }
+
+        private void JogadaEfetuada(Mensagem mensagem)
+        {
+            int x = mensagem.Posicao.Key, y = mensagem.Posicao.Value;
+            PreencheQuadrado(x, y);
+            if (mensagem.EuVenci)
             {
-                aguardandoResposta = false;
-                MessageBox.Show(string.Format("{0} recusou o desafio", mensagem.NomeJogador), "Jogo da Velha");
-                online = false;
+                MessageBox.Show(string.Format("Não foi dessa vez.\n{0} ganhou de você.", mensagem.NomeJogador), "Jogo da velha", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LiberaQuadrados(false);
             }
+            else
+                LiberaQuadrados(true);
+        }
+
+        private void JogoRecusado(Mensagem mensagem)
+        {
+            aguardandoResposta = false;
+            MessageBox.Show(string.Format("{0} recusou o desafio", mensagem.NomeJogador), "Jogo da Velha");
+            online = false;
+        }
+
+        private void JogoAceito(Mensagem mensagem)
+        {
+            aguardandoResposta = false;
+            MessageBox.Show(string.Format("{0} aceitou o desafio", mensagem.NomeJogador), "Jogo da Velha");
+            online = true;
+            lblMensagem.Text += "Jogo iniciado";
+        }
+
+        private void SolicitacaoDeJogo(Mensagem mensagem)
+        {
+            var retorno = MessageBox.Show(string.Format("{0} deseja jogar com você!\nVocê aceita?", mensagem.NomeJogador), "Jogo da Velha", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (retorno == DialogResult.Yes)
+            {
+                comunicador.AceitarJogo(true, txtNome.Text);
+                IniciaJogo(true);
+                meuSimbolo = "O";
+                online = true;
+                lblMensagem.Text += "Jogo iniciado";
+            }
+            else
+                comunicador.AceitarJogo(false, txtNome.Text);
         }
 
         private void PreencheQuadrado(int x, int y)
@@ -191,15 +219,28 @@ namespace JogoDaVelha
 
         private void IniciarJogo(object sender, EventArgs e)
         {
+            labels.ToList().ForEach(x => x.Text = string.Empty);
             if (rbOnline.Checked)
             {
                 comunicador.IniciaClient(txtIp.Text);
-                comunicador.SolicitarJogo(txtNome.Text);
-                aguardandoResposta = true;
+                try
+                {
+                    comunicador.SolicitarJogo(txtNome.Text);
+                    aguardandoResposta = true;
+                }
+                catch (SocketException ex)
+                {
+                    DialogResult resultado = DialogResult.Cancel;
+                    if (ex.SocketErrorCode == SocketError.ConnectionRefused)
+                        resultado = MessageBox.Show("Não foi possível se conectar ao amigo.\nClique em repetir para tentar novamente", "Jogo da velha", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                    if (resultado == DialogResult.Retry)
+                        IniciarJogo(sender, e);
+                }
             }
             else
             {
                 IniciaJogo(false);
+                btnIniciar.Text = "Reiniciar";
             }
         }
 
@@ -213,11 +254,7 @@ namespace JogoDaVelha
 
         private void LiberaQuadrados(bool liberar)
         {
-            foreach (var item in labels)
-            {
-                Action a = () => item.Enabled = liberar;
-                item.Invoke(a);
-            }
+            labels.ToList().ForEach(x => x.Invoke(new Action(() => x.Enabled = liberar)));
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
